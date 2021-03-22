00b79c3beb3607315b9057a01e2346b5242697fc
Rafael Miranda <rafaelmirandalobo1@gmail.com>
2021-03-22 20:36:59 -0300

    adicionando dowload

--- src/script/pesquisa.js
+++ src/script/pesquisa.js
@@ -1,99 +1,119 @@
 const dowloads = [{
+        name: '3cx phone 3cxphone 3cx-phone',
         titulo: '3CXPhone',
         imagem: 'https://raw.githubusercontent.com/RafaelMiranda21/support/main/src/imagens/apps/3CXPhone.png',
         end: 'src/dowloads/3CXPhone6.msi'
     },
     {
+        name: 'eye beam eyebeam eye-beam',
         titulo: 'eyeBeam',
         imagem: 'https://raw.githubusercontent.com/RafaelMiranda21/support/main/src/imagens/apps/eyebeam.png',
         end: 'src/dowloads/eyeBeam.rar'
     },
     {
+        name: 'office pacoteoffice microsoftoffice microsoft office pacote office',
         titulo: 'pacoteOffice',
         imagem: 'https://raw.githubusercontent.com/RafaelMiranda21/support/main/src/imagens/apps/office.png',
         end: 'src/dowloads/OfficeSetup.exe'
     },
     {
+        name: 'winrar',
         titulo: 'Winrar',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/WinRAR.png?raw=true',
         end: 'src/dowloads/winrar-x64.exe'
     },
     {
+        name: 'adobereader adobe reader adobe-reader',
         titulo: 'Adobe Reader',
         imagem: 'https://raw.githubusercontent.com/RafaelMiranda21/support/main/src/imagens/apps/adobereader.png',
         end: 'src/dowloads/readerdc.exe'
     },
     {
+        name: 'sankhya 64x sankhya64x sankhya64',
         titulo: 'Sankhya 64x',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/Sankhya.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1UhIseknf5LnH6od-S8kcwlLbjcZ836iK&export=download'
     },
     {
+        name: 'sankhya 32x sankhya32x sankhya32',
         titulo: 'Sankhya 32x',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/Sankhya.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1xYQ4G1fVxbKEPf2fdgKtml-0SxV1zx4Z&export=download'
     },
     {
+        name: 'foxitreader foxit reader foxit-reader',
         titulo: 'Foxit Reader',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/Foxit.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1ZOSsBpGY_3uz1ePp-bA_noYZInCGTEm9&export=download'
     },
     {
+        name: 'catalogo eaton catalogoeaton catalogo-eaton',
         titulo: 'Catalogo Eaton',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/eaton.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1NykLQd_QxlnUM4MQVDMdksaU7J95wcUh&export=download'
     },
     {
+        name: 'catalogo mwm catalogomwm catalogo-mwm',
         titulo: 'Catalogo MWM',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/mwm.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=102lH1C4WWXYNA93SG2hQ0JAJjeIHMdET&export=download'
     },
     {
+        name: 'catalogo ford catalogoford catalogo-ford',
         titulo: 'Catalogo Ford',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/ford.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1oR6O_YSE7NiSruGUwxX1SWx_zNQa7igx&export=download'
     },
     {
+        name: 'catalogo wolks catalogowolks catalogo-wolks',
         titulo: 'Catalogo Wolks',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/wolks.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1a_N3huRBUGfuTS5zSe1GbG3lT9W_V60B&export=download'
     },
     {
+        name: 'vmwareplayer vmware player',
         titulo: 'VMware Player',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/VMware.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1ylfC8tGRy5hw8n1WnIlpjX1xKqkAW2zX&export=download'
     },
     {
+        name: 'Libre Office x64 LibreOfficex64 LibreOffice',
         titulo: 'Libre Office x64',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/libreoffice.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1tKucQR-KCSXJEhTh_dRMsu3v0gb8JBeA&export=download'
     },
     {
+        name: 'Libre Office x86 LibreOfficex86 LibreOffice',
         titulo: 'Libre Office x86',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/libreoffice.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1mXPlkS3-BDIjMkCgdjieWVvQcxz9REDZ&export=download'
     },
     {
+        name: 'lightshot light-shot light shot',
         titulo: 'LightShot',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/lightshot.png?raw=true',
         end: 'src/dowloads/lightshot.exe'
     },
     {
+        name: 'drive booster drivebooster drive-booster',
         titulo: 'Driver Booster',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/driverBooster.png?raw=true',
         end: 'https://drive.google.com/u/1/uc?id=1nbffLH_utv0plIhoRcbst8xsKvcua1kG&export=download'
     },
     {
+        name: 'goglobal go global go-global catalogo',
         titulo: 'GO-Global',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/goGlobal.png?raw=true',
         end: 'src/dowloads/go-global.exe'
     },
     {
+        name: 'anydesk any desk any-desk',
         titulo: 'AnyDesk',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/anydesk.png?raw=true',
         end: 'src/dowloads/AnyDesk.exe'
     },
     {
+        name: 'o cam ocam o-cam',
         titulo: 'Ocam',
         imagem: 'https://github.com/RafaelMiranda21/support/blob/main/src/imagens/apps/ocam.png?raw=true',
         end: 'src/dowloads/oCam.exe'
@@ -111,7 +131,7 @@ function pesquisa(e) {
 
 function buscarPesquisa(p) {
     estiliza();
-    const dowload = dowloads.filter(d => d.titulo.toUpperCase().includes(p.toUpperCase()));
+    const dowload = dowloads.filter(d => d.name.toUpperCase().includes(p.toUpperCase()));
     if (dowload.length == 0) {
         if (!document.getElementById('mensagem-container')) {
             if (document.getElementById('dowloads-container')) {
